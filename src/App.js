4bf169a27ee85c80fbfa2f8f0627242c206efbc4
Manpreet <manpreetisar@gmail.com>
2022-04-17 18:37:21 +0530

    add react - router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import { Header, Footer } from "./components/";
+import { Header, Footer, PomoTimer } from "./components/";
 import { TaskPage } from "./pages/TaskPage/TaskPage";
 import { useTheme } from "./context/";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const { theme, setTheme } = useTheme();
@@ -9,7 +10,10 @@ function App() {
   return (
     <div className={`${theme ? "dark-mode" : "app"}`}>
       <Header />
-      <TaskPage />
+      <Routes>
+        <Route path="/" element={<TaskPage />}></Route>
+        <Route path="/timer" element={<PomoTimer />}></Route>
+      </Routes>
       <Footer />
     </div>
   );
